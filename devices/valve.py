eacb91eb8837247ad9941758e80b1d1a824fca78
scottlaverty1 <74930429+scottlaverty1@users.noreply.github.com>
2025-10-29 11:15:56 -0400

    Update valve.py

--- devices/valve.py
+++ devices/valve.py
@@ -1,6 +1,18 @@
-"""Valve device interface for AutoZahrt."""
+# ------------------------------------------------------------------------------
+# Software: AUTO_ZAHRT
+# Copyright: (C) 2025 by Professor Andrew Zahrt
+# This software is the intellectual property of Professor Andrew Zahrt
+# Contributions by graduate students Scott Laverty are acknowledged.
+# All rights reserved.
+# ------------------------------------------------------------------------------
 
-from .devices import Device
+"""
+Valve device interface for AutoZahrt.
+
+The role of this class is to control valves and all objects in this class
+"""
+
+from ..devices import Device
 import abc
 
 
@@ -8,13 +20,19 @@ class Valve(Device, abc.ABC):
     """Abstract interface for valves (on/off or positionable)."""
 
     @abc.abstractmethod
-    async def open(self) -> None:
-        """Open the valve."""
+    def move_home(self) -> None:
+        """Moves a valve to the home position."""
 
     @abc.abstractmethod
-    async def close(self) -> None:
-        """Close the valve."""
+    def go_to_position(self, pos: int) -> None:
+        """Moves a valve to the specified position."""
 
-    async def is_open(self) -> bool:
-        """Optional: return whether valve is open. Implement if available."""
-        raise NotImplementedError
+    @abc.abstractmethod
+    def check_current_position(self) -> int:
+        """Returns the current valve position as an integer."""
+
+    @abc.abstractmethod
+    def check_firmware_version(self) -> str:
+        """Returns the firmware version string of the valve controller."""
+
+#TODO : Add the multiple valve controller class
